a2da282d4c0608675678348bd77e3bd7d5e50d0a
Thomas Fox <thomasfox@Mac.powerhub>
2026-03-15 08:24:15 +0000

    your message here

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import TeamLeaderboard from "../components/TeamLeaderboard";
 import TopPlayerLeaderboard from "../components/TopPlayerLeaderboard";
-
+import logo from "../assets/masters.webp";
 
 function Home() {
   return (
     <>
+    <img className="img" src={logo} />
     <h1 className="main-title">⛳ Leaderboards</h1>
     <TeamLeaderboard />
     <TopPlayerLeaderboard />
